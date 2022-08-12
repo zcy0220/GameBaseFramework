07d562f4be3cc4f2411148bebb2f8c68bff944fb
晨杨 <690574486@qq.com>
2022-08-12 17:47:45 +0800

    事件系统

---
+++ Base/TypeId.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * 获取类型对应的唯一Id
+ */
+
+namespace GameBaseFramework.Base
+{
+    public interface ITypeId { }
+
+    public class TypeId
+    {
+        /// <summary>
+        /// TypeId
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private class Id<T> where T : ITypeId
+        {
+            public static int TypeId = -1;
+        }
+
+        /// <summary>
+        /// 唯一Id
+        /// </summary>
+        public static int _uniqueId = 0;
+
+        /// <summary>
+        /// 获取类型的Id
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static int GetId<T>() where T : ITypeId
+        {
+            if (Id<T>.TypeId < 0)
+            {
+                Id<T>.TypeId = ++_uniqueId;
+            }
+            return Id<T>.TypeId;
+        }
+    }
+}
--- Event/EventManager.cs
+++ Event/EventManager.cs
@@ -4,8 +4,9 @@
 
 using System;
 using System.Collections.Generic;
+using GameBaseFramework.Base;
 
-namespace GameFramework.Event
+namespace GameBaseFramework.Event
 {
     public class EventManager
     {
@@ -17,10 +18,55 @@ namespace GameFramework.Event
         /// <summary>
         /// 添加监听事件
         /// </summary>
-        public void AddListener<T>(Action<T> handler) where T : BaseEvent
+        public void AddListener<T>(Action<T> handler) where T : ITypeId
         {
-            //var typeId = T.GetType();
-            //if (_handlerDict.TryGetValue())
+            var typeId = TypeId.GetId<T>();
+            if (_handlerDict.TryGetValue(typeId, out var list))
+            {
+                if (!list.Contains(handler))
+                {
+                    list.Add(handler);
+                }
+            }
+            else
+            {
+                list = new List<object>() { handler };
+                _handlerDict.Add(typeId, list);
+            }
+        }
+
+        /// <summary>
+        /// 删除监听
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="handler"></param>
+        public void RemoveListener<T>(Action<T> handler) where T : ITypeId
+        {
+            var typeId = TypeId.GetId<T>();
+            if (_handlerDict.TryGetValue(typeId, out var list))
+            {
+                if (list.Contains(handler))
+                {
+                    list.Remove(handler);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 派发事件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ev"></param>
+        public void DispatchEvent<T>(T ev) where T : ITypeId
+        {
+            var typeId = TypeId.GetId<T>();
+            if (_handlerDict.TryGetValue(typeId, out var list))
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    (list[i] as Action<T>).Invoke(ev);
+                }
+            }
         }
     }
 }
